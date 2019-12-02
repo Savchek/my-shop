e99bcd0d0b005f8013cd49b85f2a7f8bc2a08ac3
Savchek <danil.753253@gmail.com>
2019-12-03 00:25:07 +0200

    Updates

---
+++ precache-manifest.a6a95cd233333dc3015ab3a31a0c0a02.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313516803f4b838082c379817e419030",
+    "url": "/my-shop/index.html"
+  },
+  {
+    "revision": "329f55577f00ec35c378",
+    "url": "/my-shop/static/js/2.190a1bcd.chunk.js"
+  },
+  {
+    "revision": "be9c7a32a81ba84a025e",
+    "url": "/my-shop/static/js/main.55190d32.chunk.js"
+  },
+  {
+    "revision": "857534ba035a97a49531",
+    "url": "/my-shop/static/js/runtime-main.a79ba330.js"
+  }
+]);
